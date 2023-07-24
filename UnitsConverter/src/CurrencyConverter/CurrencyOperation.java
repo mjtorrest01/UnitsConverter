7b0ac5e79638e6fe9368c3ec5f705e94628ab3f6
mjtorrest <michaeljtt@outlook.com>
2023-07-24 12:53:08 -0500

    Creando Funcion de Operacion -  CurrencyOperation

--- UnitsConverter/src/CurrencyConverter/CurrencyOperation.java
+++ UnitsConverter/src/CurrencyConverter/CurrencyOperation.java
@@ -1,5 +1,11 @@
 package CurrencyConverter;
 
+import javax.swing.JOptionPane;
+
 public class CurrencyOperation {
-    
+    public void convertCurrency(double value, String coin, double equivalent, String nomenclature , String symbol ) {
+        double convertdValue = value * equivalent;
+        convertdValue = (double) Math.round(convertdValue*100d/100);
+        JOptionPane.showMessageDialog(null, value + " " + coin + " = " + symbol + convertdValue + " " + nomenclature + ".");
+    }
 }
