71ec924fc6d4ea0380f78905f74e45c70d5eee62
mjtorrest <michaeljtt@outlook.com>
2023-07-23 20:17:02 -0500

    Creando Funcionalidad de operacion de distancia

--- UnitsConverter/src/DistanceConverter/DistanceOperation.java
+++ UnitsConverter/src/DistanceConverter/DistanceOperation.java
@@ -1,5 +1,23 @@
 package DistanceConverter;
 
+import javax.swing.JOptionPane;
+
 public class DistanceOperation {
     
+    public void DistanceConverter(double value, String unit, double conversionRatio, String nomenclature, double type){
+        double convertedDistance;
+
+        if (type == 1) {
+
+            convertedDistance = value * conversionRatio;
+            convertedDistance = (double) Math.round(convertedDistance*1000d)/1000;
+            JOptionPane.showMessageDialog(null, value + " " + unit + " = " + convertedDistance + nomenclature + ".");
+
+        } else if (type == 2){
+
+            convertedDistance = value / conversionRatio;
+            convertedDistance = (double) Math.round(convertedDistance*1000d)/1000;
+            JOptionPane.showMessageDialog(null, value + " " + unit + " = " + convertedDistance + nomenclature + ".");
+        }
+    }
 }
