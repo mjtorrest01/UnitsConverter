6a043becae5f21bb804e9e7691b41b48da7108c7
mjtorrest <michaeljtt@outlook.com>
2023-07-23 20:59:00 -0500

    creando funcion de convertMeasures - 1ra parte

--- UnitsConverter/src/DistanceConverter/DistanceFunctionality.java
+++ UnitsConverter/src/DistanceConverter/DistanceFunctionality.java
@@ -1,5 +1,39 @@
 package DistanceConverter;
 
+import javax.swing.JOptionPane;
+
 public class DistanceFunctionality {
     
+    DistanceOperation converter = new DistanceOperation();
+
+    public void convertMeasures (double measure) {
+
+        String distanceMeasures = (JOptionPane.showInputDialog(null, "Elija la convercion a realizar", 
+                                                                        "Converciones de Distancia", JOptionPane.QUESTION_MESSAGE, null,
+                                                                        new Object[] {  "Años Luz a Millas",
+                                                                                        "Años Luz a Kilometros",
+                                                                                        "Kilometros a Años Luz",
+                                                                                        "Kilometros a Millas",
+                                                                                        "Kilometros a Metros",
+                                                                                        "Metros a Millas",
+                                                                                        "Metros a Kilometros",
+                                                                                        "Metros a Pies",
+                                                                                        "Metros a Pulgadas",
+                                                                                        "Metros a Centimetros",
+                                                                                        "Pies a Metros",
+                                                                                        "Pies a Pulgadas",
+                                                                                        "Pies a Centimetros",
+                                                                                        "Pulgadas a Metros",
+                                                                                        "Pulgadas a Pies",
+                                                                                        "Pulgadas a Centimetros",
+                                                                                        "Decimetros a Centimetros",
+                                                                                        "Decimetros a Milimetros",
+                                                                                        "Decimetros a Metros",
+                                                                                        "Centimetros a Pulgadas",
+                                                                                        "Centimetros a Decimetros",
+                                                                                        "Centimetros a Milimetros",
+                                                                                        "Milimetros a Decimetros",
+                                                                                        "Milimetros a Centimetros" },
+                                                                                        "Seleccion")).toString();
+    }
 }
