46995c4def496aee6132b744c9dc4be82c20aded
mjtorrest <michaeljtt@outlook.com>
2023-07-26 11:47:54 -0500

    Creando Funciones | SpeedConverter

--- UnitsConverter/src/SpeedConverter/SpeedFunctionality.java
+++ UnitsConverter/src/SpeedConverter/SpeedFunctionality.java
@@ -1,5 +1,44 @@
 package SpeedConverter;
 
+import javax.swing.JOptionPane;
+
 public class SpeedFunctionality {
+
+    double nudoToMph = 1.151;
+    double nudoToKmh = 1.852;
+    double mphToKmh = 1.60934;
+
+    SpeedOperation converter = new SpeedOperation();
     
+    public void velocityConverter(double valueSpeed) {
+        String speedOption = (JOptionPane.showInputDialog(null, "Elija la convercion a realizar", "Opciones de convercion", 
+        JOptionPane.QUESTION_MESSAGE, null, 
+        new Object[] {  "Nudo a mph",
+                        "Nudo a Km/h",
+                        "mph a Nudo",
+                        "mph a Km/h",
+                        "Km/h a Nudo",
+                        "Km/h a mph" }, "Selecion")).toString();
+
+        switch (speedOption) {
+            case "Nudo a mph":
+                converter.speedConverter(valueSpeed, "Nudos", nudoToMph, "mph", "m"); // multiplica
+                break;
+            case "Nudo a Km/h":
+                converter.speedConverter(valueSpeed, "Nudos", nudoToKmh, "Km/h", "m"); // mutiplica
+                break;
+            case "mph a Nudo":
+                converter.speedConverter(valueSpeed, "mph", nudoToMph, "Nudo", "d"); // dividir
+                break;
+            case "mph a Km/h":
+                converter.speedConverter(valueSpeed, "mph", mphToKmh, "Km/h", "m"); // multiplica
+                break;
+            case "Km/h a Nudo":
+                converter.speedConverter(valueSpeed, "Km/h", nudoToKmh, "Nudo", "d"); // divide
+                break;
+            case "Km/h a mph":
+                converter.speedConverter(valueSpeed, "Km/h", mphToKmh, "mph", "d"); // divide
+                break;
+        }
+    }
 }
--- UnitsConverter/src/SpeedConverter/SpeedOperation.java
+++ UnitsConverter/src/SpeedConverter/SpeedOperation.java
@@ -1,5 +1,22 @@
 package SpeedConverter;
 
+import javax.swing.JOptionPane;
+
 public class SpeedOperation {
-    
+    public void speedConverter(double value, String unit, double conversionRatio, String nomenclature, String operation){
+        double convertedDistance;
+
+        if (operation == "m") {
+
+            convertedDistance = value * conversionRatio;
+            convertedDistance = (double) Math.round(convertedDistance*1000d)/1000;
+            JOptionPane.showMessageDialog(null, value + " " + unit + " = " + convertedDistance + nomenclature + ".");
+
+        } else if (operation == "d"){
+
+            convertedDistance = value / conversionRatio;
+            convertedDistance = (double) Math.round(convertedDistance*1000d)/1000;
+            JOptionPane.showMessageDialog(null, value + " " + unit + " = " + convertedDistance + nomenclature + ".");
+        }
+    }
 }
